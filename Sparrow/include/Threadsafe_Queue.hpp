8ef5166aaf4166bac260935b603a9ca673a7bc8f
n3in <n3in2020@163.com>
2020-06-14 13:03:01 +0800

    add Threadsafe_Queue.hpp

---
+++ Sparrow/include/Threadsafe_Queue.hpp
@@ -0,0 +1,34 @@
+#ifndef THREAD_SAFE_QUEUE
+#define THREAD_SAFE_QUEUE
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <memory>
+
+#include "Threadsafe_Queue.hpp"
+
+template<typename T>
+class TSQueue
+{
+public:
+    void push(T value)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        q.push(std::move(value));
+        cv.notify_one();
+    }
+
+    void wait_and_pop(T &value)
+    {
+        std::unique_lock<std::mutex> lock(mutex);
+        cv.wait(lock, [this] { return !q.empty();});
+        value = std::move(q.front());
+        q.pop();
+    }
+
+private:
+    std::mutex mutex;
+    std::queue<T> q;
+    std::condition_variable cv;
+};
+#endif
