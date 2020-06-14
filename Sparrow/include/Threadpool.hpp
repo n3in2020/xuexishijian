57abb19b99badaf54f0a018ef0f87ed68686bcf5
n3in <n3in2020@163.com>
2020-06-14 13:02:43 +0800

    add Threadpool.hpp

---
+++ Sparrow/include/Threadpool.hpp
@@ -0,0 +1,92 @@
+#ifndef THREAD_POOL_H
+#define THREAD_POOL_H
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <functional>
+#include <vector>
+#include <atomic>
+#include <future>
+#include <memory>
+class ThreadPool
+{
+public:
+    ThreadPool(int N = std::thread::hardware_concurrency()) : done{false}, busy(0)
+    {
+        // 创建线程并令其执行work函数
+        for (unsigned i = 0; i < N; ++i)
+        {
+            threads.push_back(std::thread(&ThreadPool::work, this));
+        }
+    }
+
+    ~ThreadPool()
+    {
+        // 线程池工作完成并通知所有线程
+        done = true;
+        cv.notify_all();
+        // 等待线程结束
+        for (int i = 0; i < threads.size(); ++i)
+        {
+            if (threads[i].joinable())
+                threads[i].join();
+        }
+    }
+
+    template <typename Func, typename... Args>
+    void submit(Func &&func, Args &&... args)
+    {
+        std::lock_guard<std::mutex> lock(m);
+        tasks.push(std::bind(std::forward<Func>(func), std::forward<Args>(args)...));
+        cv.notify_one();
+    }
+
+    void waitFinished()
+    {
+        std::unique_lock<std::mutex> lock(m);
+        cv_finished.wait(lock, [this] { return tasks.empty() && (busy == 0); });
+    }
+
+    const int size() const { return threads.size(); }
+private:
+    std::atomic<bool> done;
+    mutable std::mutex m;
+    std::condition_variable cv;
+    std::queue<std::function<void()>> tasks;
+    std::vector<std::thread> threads;
+    std::atomic<unsigned> busy;
+    std::condition_variable cv_finished;
+    // 线程工作
+    void work()
+    {
+        std::function<void()> task;
+        while (true)
+        {
+            // 获取队列中的任务
+            {
+                std::unique_lock<std::mutex> lock(m);
+                // 等待队列中的新任务或者线程池工作完成
+                cv.wait(lock, [this] { return !tasks.empty() || done.load(); });
+                // 如果线程池工作完成，结束线程工作
+                if (done.load())
+                {
+                    return;
+                }
+                else
+                {
+                    // 获取队列中的新任务
+                    ++busy;
+                    task = std::move(tasks.front());
+                    tasks.pop();
+                }
+            }
+
+            // 执行任务
+            task();
+            --busy;
+            cv_finished.notify_all();
+        }
+    }
+};
+#endif
